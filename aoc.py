264ace8b66c464427c556dbb782ede557b441f67
Johan Svanström <svanis71@gmail.com>
2017-12-02 09:10:39 +0100

    Day 1

---
+++ aoc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from day1 import Day1
+
+if __name__ == '__main__':
+    day1 = Day1()
+    print("Day1 pt 1: ")
+    print("==============")
+    print("Answer is: %d" % day1.part1())
+    print("Day1 pt 2: ")
+    print("==============")
+    print("Answer is: %d" % day1.part2())
