2855680a486c54589e3a3aef0d5e98035cca6c59
Johan Svanström <svanis71@gmail.com>
2017-12-04 20:34:00 +0100

    Day3 complete

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,38 @@
 import math
+from collections import defaultdict
 
 class Day3:
+    def getSum(self, curpos, spiralen):
+        curX, curY = curpos
+        sum = spiralen[(curX + 1, curY)] # e
+        sum += spiralen[(curX - 1, curY)] # w
+        sum += spiralen[(curX, curY - 1)] # n
+        sum += spiralen[(curX, curY + 1)] # s
+        sum += spiralen[(curX + 1, curY + 1)] # se
+        sum += spiralen[(curX - 1, curY - 1)] # nw
+        sum += spiralen[(curX + 1, curY - 1)] # ne
+        sum += spiralen[(curX - 1, curY + 1)] # sw
+        return sum
+
+    def nextPos(self, cur, spiralen):
+        curX, curY = cur
+        
+        newPos = ()
+        # Up
+        if spiralen[(curX - 1, curY)] != 0 and spiralen[(curX, curY -1)] == 0:
+            newPos = (curX, curY - 1)
+            # Left
+        elif spiralen[(curX, curY + 1)] != 0 and spiralen[(curX - 1, curY)] == 0:
+            newPos = (curX - 1, curY)
+            # Down
+        elif spiralen[(curX + 1, curY)] != 0 and spiralen[(curX, curY + 1)] == 0:
+            newPos = (curX, curY + 1)
+            # Right
+        else:
+            newPos = (curX + 1, curY)
+            
+        return newPos
+
     def part1(self):
 
         target = 289326
@@ -22,4 +54,17 @@ class Day3:
         return diff
     
     def part2(self):
-        return 0
+        target = 289326
+        val = 1
+        spiral = defaultdict(int)
+        # x,y
+        pos = (0,0)
+        spiral[pos] = 1
+        pos = (1,0)
+        spiral[pos] = 1
+        
+        while spiral[pos] < target:
+            pos = self.nextPos(pos, spiral)
+            spiral[pos] =  self.getSum(pos, spiral)
+        return spiral[pos]
+            
